15a3465bb45983e9b76dd267ec910e2935edda82
seekers <seekersMan@qq.com>
2022-08-23 13:44:22 +0800

    fix: delete the package file

--- packages/core/src/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const LOWEST_NODE_VERSION = '12.0.0';
-const DEFAULT_CLI_HOME = '.moderate-cli';
-exports.default = {
-    LOWEST_NODE_VERSION,
-    DEFAULT_CLI_HOME
-};
