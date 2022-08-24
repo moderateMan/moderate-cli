e39cec72c3e3aaad8eff014203f2098db517754d
qanglee <545540710@qq.com>
2022-08-24 10:54:36 +0800

    refa: 去除冗余代码

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,5 @@
 #! /usr/bin/env node
 // moderate-cli
-let isDebug = process.argv.includes("-d")
+
 const pkg = require("../package.json")
-if (!isDebug) {
-    require("@moderate-cli/core").default(pkg)
-} else {
-    require("../packages/core/dist").default(pkg)
-}
+require("@moderate-cli/core").default(pkg)
